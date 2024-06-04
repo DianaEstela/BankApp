264647ec1f4e45648c554f8ff5802c13055e8370
diana <diana@DIANAALONSO>
2024-06-03 20:37:03 -0600

    Agregar archivos de proyecto.

---
+++ BankApp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BankApp.Models;
+
+namespace BankApp.Controllers
+{
+    public class HomeController : Controller
+    {
+       [Route("/")]
+        public IActionResult Index()
+        {
+            Response.StatusCode = 200;
+            return Content("Welcome to my banl account");
+        }
+        [Route ("/account-details")]
+        public JsonResult Details()
+        {
+            Detalle  detalle = new()
+            {
+                AccountNumber =1001,
+                AccountHolderName= "diana",
+                CurrentBalance = 5000,
+                    
+            };
+            return Json(detalle);
+        }
+        [Route("account-statement")]
+        public VirtualFileResult Statement()
+        {
+            return File("CV Diana Estela Alonso López.pdf", "application/pdf");
+
+        }
+
+        [Route ("get - current - balance")]
+        public IActionResult Balance()
+        {
+            if (!Request.Query.ContainsKey("accountNumber"))
+            {
+                return NotFound("account number is not supplied"); 
+            }
+            if (string.IsNullOrEmpty(Convert.ToString(Request.Query["accountNumber"])))
+            {
+                return BadRequest("account cant´n be null or empty");
+            }
+            int accountNum = Convert.ToUInt16(ControllerContext.HttpContext.Request.Query["accountNumber"]);
+
+            if( accountNum != 1001)
+            {
+                return BadRequest("Account Number should be 1001");
+            }
+            return Content(accountNum.ToString());
+        }
+
+
+    }
+}
---
+++ BankApp/Models/Detalle.cs
@@ -0,0 +1,10 @@
+﻿namespace BankApp.Models
+{
+    public class Detalle
+    {
+        public int AccountNumber { get; set; }
+        public string? AccountHolderName { get; set; }
+        public int CurrentBalance { get; set; }
+        
+    }
+}
